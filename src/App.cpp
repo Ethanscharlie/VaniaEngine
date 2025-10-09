3833fcba3c5c43c8db9fd5e0a516dfcfd1c5aa71
Ethanscharlie <ethanscharlie@protonmail.com>
2025-10-09 17:24:13 -0600

    Working with app class

---
+++ src/App.cpp
@@ -0,0 +1,74 @@
+#include "App.hpp"
+#include <cstdlib>
+
+namespace Vania {
+App::App() {
+  initSDL();
+  createWindow();
+  initImGui();
+}
+
+App::~App() {
+  ImGui_ImplSDLRenderer3_Shutdown();
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
+
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  SDL_Quit();
+}
+
+void App::update() {
+  SDL_PollEvent(&event);
+  ImGui_ImplSDL3_ProcessEvent(&event);
+
+  if (event.type == SDL_EVENT_QUIT) {
+    std::exit(1);
+  }
+
+  ImGui_ImplSDLRenderer3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+
+  ImGui::ShowDemoWindow();
+
+  ImGui::Render();
+  SDL_SetRenderScale(renderer, io->DisplayFramebufferScale.x,
+                     io->DisplayFramebufferScale.y);
+  SDL_RenderClear(renderer);
+  ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+  SDL_RenderPresent(renderer);
+}
+
+void App::initSDL() {
+  const int sdlInitSuccess = SDL_Init(SDL_INIT_VIDEO);
+  if (!sdlInitSuccess) {
+    throw std::runtime_error(
+        std::format("Couldn't initialize SDL: %s\n", SDL_GetError()).c_str());
+  }
+}
+
+void App::createWindow() {
+  const int windowCreationSucess =
+      SDL_CreateWindowAndRenderer("Demo", WINDOW_WIDTH, WINDOW_HEIGHT,
+                                  SDL_WINDOW_RESIZABLE, &window, &renderer);
+  if (!windowCreationSucess) {
+    throw std::runtime_error(
+        std::format("Couldn't initialize SDL window: %s\n", SDL_GetError())
+            .c_str());
+  }
+}
+
+void App::initImGui() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  io = &ImGui::GetIO();
+  (void)io;
+
+  ImGui::StyleColorsDark();
+
+  ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer3_Init(renderer);
+}
+
+}; // namespace Vania
---
+++ src/App.hpp
@@ -0,0 +1,29 @@
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+#include <SDL3/SDL.h>
+#include <print>
+#include <stdexcept>
+#include <unistd.h>
+
+namespace Vania {
+class App {
+  const int WINDOW_WIDTH = 1920;
+  const int WINDOW_HEIGHT = 1080;
+
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  SDL_Event event;
+  ImGuiIO *io;
+
+public:
+  App();
+  ~App();
+  void update();
+
+private:
+  void initSDL();
+  void createWindow();
+  void initImGui();
+};
+}; // namespace Vania
