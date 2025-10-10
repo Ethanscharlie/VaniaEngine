e8cfe728df394635ae86ebcc1ea1bc93841944fa
Ethanscharlie <ethanscharlie@protonmail.com>
2025-10-09 20:21:37 -0600

    Working entity selector

--- src/EntityPanel.cpp
+++ src/EntityPanel.cpp
@@ -1,11 +1,29 @@
 #include "EntityPanel.hpp"
 #include "imgui.h"
+#include <format>
 
 namespace Vania {
-EntityPanel::EntityPanel() {}
+EntityPanel::EntityPanel() { entities = {"Ethan", "Connor"}; }
 
 void EntityPanel::update() {
   ImGui::Begin("Entity");
+
+  static int selected = 0;
+  {
+    ImGui::BeginChild("Entity Selection", ImVec2(150, 0),
+                      ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeX);
+    for (int i = 0; i < entities.size(); i++) {
+      const std::string &entity = entities[i];
+      if (ImGui::Selectable(entity.c_str(), selected == i,
+                            ImGuiSelectableFlags_SelectOnNav))
+        selected = i;
+    }
+    ImGui::EndChild();
+  }
+  ImGui::SameLine();
+
+  ImGui::Text(std::format("Name: {}", entities[selected]).c_str());
+
   ImGui::End();
 }
 } // namespace Vania
--- src/EntityPanel.hpp
+++ src/EntityPanel.hpp
@@ -1,7 +1,11 @@
 #include "IPanel.hpp"
+#include <string>
+#include <vector>
 
 namespace Vania {
 class EntityPanel : public IPanel {
+  std::vector<std::string> entities;
+
 public:
   EntityPanel();
   void update() override;
