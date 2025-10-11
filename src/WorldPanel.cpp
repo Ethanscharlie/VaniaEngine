e32de1dab89a1712d0dd1b49fe9c3d3161a73118
Ethanscharlie <ethanscharlie@protonmail.com>
2025-10-11 14:14:22 -0600

    refactored window panel

--- src/WorldPanel.cpp
+++ src/WorldPanel.cpp
@@ -12,60 +12,66 @@ WorldPanel::WorldPanel(GameData &gameData) : gameData(gameData) {}
 void WorldPanel::update() {
   ImGui::Begin("World");
 
-  static ImVec2 scrolling(0.0f, 0.0f);
-
-  // Using InvisibleButton() as a convenience 1) it will advance the layout
-  // cursor and 2) allows us to use IsItemHovered()/IsItemActive()
-  ImVec2 canvas_p0 =
-      ImGui::GetCursorScreenPos(); // ImDrawList API uses screen coordinates!
-  ImVec2 canvas_sz =
-      ImGui::GetContentRegionAvail(); // Resize canvas to what's available
-  if (canvas_sz.x < 50.0f)
-    canvas_sz.x = 50.0f;
-  if (canvas_sz.y < 50.0f)
-    canvas_sz.y = 50.0f;
-  ImVec2 canvas_p1 =
-      ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
-
-  // Draw border and background color
-  ImGuiIO &io = ImGui::GetIO();
-  ImDrawList *draw_list = ImGui::GetWindowDrawList();
-  draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
-  draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+  calculateCanvasPositionValues();
 
   // This will catch our interactions
   ImGui::InvisibleButton("canvas", canvas_sz,
                          ImGuiButtonFlags_MouseButtonLeft |
                              ImGuiButtonFlags_MouseButtonRight);
-  const bool is_hovered = ImGui::IsItemHovered(); // Hovered
-  const bool is_active = ImGui::IsItemActive();   // Held
-  const ImVec2 origin(canvas_p0.x + scrolling.x,
-                      canvas_p0.y + scrolling.y); // Lock scrolled origin
-  const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x,
-                                   io.MousePos.y - origin.y);
+
+  findWindowInputState();
 
   if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, -1.0f)) {
+    ImGuiIO &io = ImGui::GetIO();
     scrolling.x += io.MouseDelta.x;
     scrolling.y += io.MouseDelta.y;
   }
 
-  // Draw grid + all lines in the canvas
-  draw_list->PushClipRect(canvas_p0, canvas_p1, true);
-  if (true) {
-    const float GRID_STEP = 64.0f;
-    for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x;
-         x += GRID_STEP)
-      draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y),
-                         ImVec2(canvas_p0.x + x, canvas_p1.y),
-                         IM_COL32(200, 200, 200, 40));
-    for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y;
-         y += GRID_STEP)
-      draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y),
-                         ImVec2(canvas_p1.x, canvas_p0.y + y),
-                         IM_COL32(200, 200, 200, 40));
-  }
-  draw_list->PopClipRect();
+  draw();
 
   ImGui::End();
 }
+
+void WorldPanel::findWindowInputState() {
+  ImGuiIO &io = ImGui::GetIO();
+  is_hovered = ImGui::IsItemHovered(); // Hovered
+  is_active = ImGui::IsItemActive();   // Held
+  origin = {canvas_p0.x + scrolling.x,
+            canvas_p0.y + scrolling.y}; // Lock scrolled origin
+  mouse_pos_in_canvas = {io.MousePos.x - origin.x, io.MousePos.y - origin.y};
+}
+
+void WorldPanel::calculateCanvasPositionValues() {
+  // Using InvisibleButton() as a convenience 1) it will advance the layout
+  // cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+  canvas_p0 =
+      ImGui::GetCursorScreenPos(); // ImDrawList API uses screen coordinates!
+  canvas_sz =
+      ImGui::GetContentRegionAvail(); // Resize canvas to what's available
+  if (canvas_sz.x < 50.0f)
+    canvas_sz.x = 50.0f;
+  if (canvas_sz.y < 50.0f)
+    canvas_sz.y = 50.0f;
+  canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+}
+
+void WorldPanel::draw() {
+  ImDrawList *draw_list = ImGui::GetWindowDrawList();
+  draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+  draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+  // Draw grid + all lines in the canvas
+  draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+  const float GRID_STEP = 64.0f;
+  for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+    draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y),
+                       ImVec2(canvas_p0.x + x, canvas_p1.y),
+                       IM_COL32(200, 200, 200, 40));
+  for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+    draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y),
+                       ImVec2(canvas_p1.x, canvas_p0.y + y),
+                       IM_COL32(200, 200, 200, 40));
+  draw_list->PopClipRect();
+}
+
 } // namespace Vania
--- src/WorldPanel.hpp
+++ src/WorldPanel.hpp
@@ -1,13 +1,30 @@
 #include "GameDataStructs.hpp"
 #include "IPanel.hpp"
+#include "imgui.h"
 #include <vector>
 
 namespace Vania {
 class WorldPanel : public IPanel {
   GameData &gameData;
 
+  ImVec2 scrolling = {0.0f, 0.0f};
+
+  ImVec2 canvas_p0;
+  ImVec2 canvas_sz;
+  ImVec2 canvas_p1;
+
+  bool is_hovered;
+  bool is_active;
+  ImVec2 origin;
+  ImVec2 mouse_pos_in_canvas;
+
 public:
   WorldPanel(GameData &gameData);
   void update() override;
+
+private:
+  void calculateCanvasPositionValues();
+  void findWindowInputState();
+  void draw();
 };
 } // namespace Vania
