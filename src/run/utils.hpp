1e507a0a3d01c90f34b0e3272350ed1c53f0e2cf
Ethanscharlie <ethanscharlie@protonmail.com>
2025-10-24 15:32:21 -0600

    Removed the old getProp functions

--- src/run/utils.hpp
+++ src/run/utils.hpp
@@ -7,21 +7,6 @@
 #include "SDL3/SDL_mouse.h"
 
 namespace Vania::utils {
-inline void setPropStr(Entity& entity, const std::string& key, const std::string& value) {
-  // entity.entityDefOverride.properties[key] = Property(0.0f, value);
-}
-
-inline void setPropNum(Entity& entity, const std::string& key, float value) {
-  // entity.entityDefOverride.properties[key] = Property(value, "");
-}
-
-inline std::string& getPropStr(Entity& entity, const std::string& key) {
-  // return entity.entityDefOverride.properties[key].text;
-}
-
-inline float getPropNum(Entity& entity, const std::string& key) {
-  // return entity.entityDefOverride.properties[key].number;
-}
 
 inline bool getButtonHeld(const std::string& button) {
   std::string b = button;
@@ -90,10 +75,6 @@ inline void lookAt(Entity& entity, float targetX, float targetY) {
 }
 
 inline void exposeAll(sol::state& lua) {
-  lua.set_function("setPropStr", setPropStr);
-  lua.set_function("setPropNum", setPropNum);
-  lua.set_function("getPropStr", getPropStr);
-  lua.set_function("getPropNum", getPropNum);
   lua.set_function("getButtonHeld", getButtonHeld);
   lua.set_function("getMouseButtonHeld", getMouseButtonHeld);
   lua.set_function("moveForward", moveForward);
